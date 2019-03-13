e5933efc8b10d0df2ae348a3c523e583c105fbb4
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2019-03-13 13:05:45 +0530

    Add files via upload

---
+++ min_ele.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,arr[1000];
+    cin>>n;
+	if(n>0)
+	{
+    for(int i=0;i<n;i++){
+	   cin>>arr[i];
+    }
+    int* i1; 
+    i1 = std::min_element(arr,arr+n); 
+  
+    cout <<*i1; 
+	}
+	else{
+	cout<<"Invalid Size";
+    return 0;
+}
