dd9d7ce193d913836ce88a17fd85add6dadd1cc6
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2019-03-17 18:45:59 +0530

    Add files via upload

---
+++ sort.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,arr[10];
+    cin>>n;
+	if(n>0)
+	{
+    for(int i=0;i<n;i++){
+	   cin>>arr[i];
+    }
+    sort(arr, arr+n);
+    for(int i=0;i<n;i++){
+	   cout<<arr[i]<<" ";
+    }
+	}
+	else
+	{
+		cout<<"Invalid size";
+    return 0;
+}
